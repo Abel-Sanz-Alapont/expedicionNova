29c881f8dbdd8c35c6429d4634d656ddc9068b91
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-02-16 17:03:34 +0100

    Modificando el formulario
    Añadido un select para que solo puedan tener estas opciones

--- views/form.php
+++ views/form.php
@@ -2,24 +2,41 @@
         <hr>
         <!--Formulario Crear-->
 
-        <form method="POST" action="index.php?accion=crear";>
+        <form method="POST" action="index.php?accion=crear" ;>
             ID:
             <input type="text" name="id" required><br>
 
             NOMBRE:
-            <input type="text" name="nombre">
+            <input type="text" name="nombre"><br>
 
             PLANETA ORIGEN:
             <input type="text" name="planetaOrigen"><br>
 
             NIVEL ESTABILIDAD:
-            <input type="number" min="1" max="10" name="nivelEstabilidad">
+            <input type="number" min="1" max="10" name="nivelEstabilidad"><br>  
 
-            DIETA:
-            <input type="text" name="dieta"><br>
-            DUREZA:
-            
-            <input type="text" name="dureza"><br>
+            <label>DIETA (Si es una Forma de Vida):</label>
+            <select name="dieta">
+                <option value="">No es una Forma de vida</option>
+                <option value="Silicio">Silicio</option>
+                <option value="Energia">Energía</option>
+                <option value="Carbono">Carbono</option>
+            </select>
+            <br>
+            <label>DUREZA (Si es un Mineral):</label>
+            <select name="dureza">
+                <option value="">No es un Mineral</option>
+                <option value="talco">Talco</option>
+                <option value="yeso">Yeso</option>
+                <option value="calcita">Calcita</option>
+                <option value="fluorita">Fluorita</option>
+                <option value="cuarzo">Cuarzo</option>
+                <option value="topacio">Topacio</option>
+                <option value="corindon">Corindon</option>
+                <option value="diamante">Diamante</option>     
+            </select>
+            <br>
+
             ANTIGUEADAD:
 
             <input type="text" name="antiguedad"><br>

499fd2c8684c25e25bc8a4008d2fae11f66e5837
Abel Sanz Alapont <absaal@alumnatflorida.es>
2026-02-19 15:20:00 +0100

    Añadiendo clases y div a form.php

--- views/form.php
+++ views/form.php
@@ -5,50 +5,65 @@
 
             <link rel="stylesheet" href="css/estilo.css">
         </head>
-        <h1>FORMULARIO PARA AÑADIR ENTIDADES </h1>
-        <hr>
+        <div>
+            <h1>FORMULARIO PARA AÑADIR ENTIDADES </h1>
+            <hr>
+        </div>
+
         <!--Formulario Crear-->
+        <div class="accionesFormulario">
+            <form method="POST" action="index.php?accion=crear" id="form" ;>
+                <div class="fila-formulario">
+                    <label>ID</label>
+                    <input type="text" name="id" required><br>
+                </div>
+                <div class="fila-formulario">
+                    <label>NOMBRE</label>
+                    <input type="text" name="nombre"><br>
+                </div >
+                <div class="fila-formulario">
+                    <label>PLANETA ORIGEN:</label>
+                    <input type="text" name="planetaOrigen"><br>
+                </div>
+                <div class="fila-formulario">
+                    <label>NIVEL ESTABILIDAD:</label>
+                    <input type="number" min="1" max="10" name="nivelEstabilidad"><br>
+                </div>
+                <div class="fila-formulario">
+                    <label>DIETA (Si es una Forma de Vida):</label>
+                    <select name="dieta">
+                        <option value="">No es una Forma de vida</option>
+                        <option value="Silicio">Silicio</option>
+                        <option value="Energia">Energía</option>
+                        <option value="Carbono">Carbono</option>
+                    </select>
+                    <br>
+                </div class="fila-formulario">
+                <label>DUREZA (Si es un Mineral):</label>
+                <select name="dureza">
+                    <option value="">No es un Mineral</option>
+                    <option value="talco">Talco</option>
+                    <option value="yeso">Yeso</option>
+                    <option value="calcita">Calcita</option>
+                    <option value="fluorita">Fluorita</option>
+                    <option value="cuarzo">Cuarzo</option>
+                    <option value="topacio">Topacio</option>
+                    <option value="corindon">Corindon</option>
+                    <option value="diamante">Diamante</option>
+                </select>
+                <br>
+                <div class="fila-formulario">
+                    <label>ANTIGUEADAD:</label>
+                    <input type="text" name="antiguedad"><br>
 
-        <form method="POST" action="index.php?accion=crear" id="form";>
-            ID:
-            <input type="text" name="id" required><br>
+                </div>
+                <div class="botones-formulario">
+                    <button type="submit">Agregar</button>
 
-            NOMBRE:
-            <input type="text" name="nombre"><br>
+                </div>
+            </form>
+            <a href="index.php">Volver</a>
+        </div>
+        <div>
 
-            PLANETA ORIGEN:
-            <input type="text" name="planetaOrigen"><br>
-
-            NIVEL ESTABILIDAD:
-            <input type="number" min="1" max="10" name="nivelEstabilidad"><br>
-
-            <label>DIETA (Si es una Forma de Vida):</label>
-            <select name="dieta">
-                <option value="">No es una Forma de vida</option>
-                <option value="Silicio">Silicio</option>
-                <option value="Energia">Energía</option>
-                <option value="Carbono">Carbono</option>
-            </select>
-            <br>
-            <label>DUREZA (Si es un Mineral):</label>
-            <select name="dureza">
-                <option value="">No es un Mineral</option>
-                <option value="talco">Talco</option>
-                <option value="yeso">Yeso</option>
-                <option value="calcita">Calcita</option>
-                <option value="fluorita">Fluorita</option>
-                <option value="cuarzo">Cuarzo</option>
-                <option value="topacio">Topacio</option>
-                <option value="corindon">Corindon</option>
-                <option value="diamante">Diamante</option>
-            </select>
-            <br>
-
-            ANTIGUEADAD:
-
-            <input type="text" name="antiguedad"><br>
-
-            <button type="submit">Agregar</button>
-
-        </form>
-        <a href="index.php">Volver</a>
+        </div>
